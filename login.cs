f9e52f84ce8abfa317e1644ec5006845d38f6856
slqt <hzqt210@gmail.com>
2019-01-30 22:14:00 +0800

    Update login.cs

--- login.cs
+++ login.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class login: MonoBehaviour
 {
-    // Start is called before the first frame update
     string text = "";
 
     string myName = "";
@@ -13,18 +12,10 @@ public class login: MonoBehaviour
 
     {
         Rect area = new Rect(100, 100,200 , 200);
-        //用标签显示文本
         GUILayout.BeginArea(area);
-
         GUILayout.Label("请输入你的名字：");
 
-        //用文本区域输入名字
-
         text = GUILayout.TextField(text);
-
-        //
-
-        
         if (GUILayout.Button("提交"))
 
         {
@@ -32,8 +23,6 @@ public class login: MonoBehaviour
             myName = text;
 
         }
-
-        //当myName不为空的时候，说明我们已经提交了名字，则显示名字
 
         if (!string.IsNullOrEmpty(myName))
 
